e98e83eb1d7e9c8f3da5bb4b2535ca4fc191c266
Tim Buschtoens <tbuschto@eclipsesource.com>
2018-12-10 17:48:00 +0100

    Add options to pin good and bad certificates

--- src/app.js
+++ src/app.js
@@ -1,13 +1,50 @@
-const {ui, Composite, ScrollView, Button, TextView, TextInput} = require('tabris');
+const {ui, app, Composite, ScrollView, Button, ToggleButton, TextView, TextInput} = require('tabris');
 
-createButton('https://badssl.com/');
-createButton('https://self-signed.badssl.com/');
+createUrlButton('https://badssl.com/');
+createUrlButton('https://self-signed.badssl.com/');
+createUrlButton('https://freegeoip.net/');
+createUrlButton('http://freegeoip.net/');
+createUrlButton('https://google.com/');
+createUrlButton('http://google.com/');
 
-function createButton(url) {
+new Button({
+  left: 10, top: 'prev() 10', right: 10,
+  text: 'pin'
+}).on({select: pin}).appendTo(ui.contentView);
+
+new Button({
+  left: 10, top: 'prev() 10', right: 10,
+  text: 'unpin'
+}).on({select: unpin}).appendTo(ui.contentView);
+
+new Button({
+  left: 10, top: 'prev() 10', right: 10,
+  text: 'badPin'
+}).on({select: badPin}).appendTo(ui.contentView);
+
+function createUrlButton(url) {
   new Button({
     left: 10, top: 'prev() 10', right: 10,
     text: url
   }).on({select: doRequest}).appendTo(ui.contentView);
+}
+
+function pin() {
+  app.pinnedCertificates = [
+    {host: '*.badssl.com',  hash: 'sha256/9SLklscvzMYj8f+52lp5ze/hY0CFHyLSPQzSpYYIBm8=', algorithm: 'ECDSA256'},
+    {host: 'freegeoip.net', hash: 'sha256/+SVYjThgePRQxQ0e8bWTQDRtPYR/xBRufqyMoeaWteo=', algorithm: 'ECDSA256'}
+  ];
+}
+
+function unpin() {
+  app.pinnedCertificates = [];
+}
+
+function badPin() {
+  app.pinnedCertificates = [
+    {host: '*.badssl.com',  hash: 'sha256/9SLklscvzMYj8f+52lp5zd/hY0CFHyLSPQzSpYYIBm8=', algorithm: 'ECDSA256'},
+    {host: 'freegeoip.net', hash: 'sha256/+SVYjThgePRQxQ0e8bWTQgRtPYR/xBRufqyMoeaWteo=', algorithm: 'ECDSA256'}
+  ];
 }
 
 function doRequest(ev) {
